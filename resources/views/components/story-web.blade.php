6155c335fc22b4ef510413ae4f634edcf3087bd8
Ali Sharifi Neyestani <sh4rifi@gmail.com>
2021-02-13 14:49:27 +0330

    Work on webApplication

--- resources/views/components/story-web.blade.php
+++ resources/views/components/story-web.blade.php
@@ -1,7 +1,8 @@
-<section class="section section-story section-story-19">
+<section class="section section-story section-story-right section-story-19">
     <header class="el-heading el-heading-2">
         <h2 class="el-big-title">
             <div>
+
                 @for ($i = 1; $i < 10; $i++)
                     <div>
                         <span>{{$title}}</span>
@@ -12,18 +13,22 @@
     </header>
     <div>
         <!--Item-->
-        @foreach($items as $item)
 
 
+        @foreach($items as $key => $item)
             <article class="el-item story-item">
                 <div class="display-center">
                     <div class="container">
                         <div class="row row-0">
-                            <div class="col-12 col-12 col-md-6 col-lg-6 col-lg-6 {{$item['offset'] ?? ''}}">
+                            <div class="col-12 col-12 col-md-6 col-lg-6 col-story">
                                 <div class="detail">
                                     <header class="el-heading el-heading-1">
-                                        <h3>{{$item['title'] ?? ''}}</h3>
-                                        <p class="mb-50">{{$item['body'] ?? ''}}</p>
+                                        <h3>
+                                            @foreach($item['title'] as $title)
+                                                <span>{{$title}}</span><br/>
+                                            @endforeach
+                                        </h3>
+                                        <p class="mb-50">{{$item['body']}}</p>
                                         <a href="#" class="button button-xl button-radius button-light">
                                             <span class="text">LEARN MORE</span>
                                         </a>
@@ -33,7 +38,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="image {{$item['bg_color'] ?? ''}}">
+                <div class="image bg-color-{{$key}}">
                     <div class="el-shape">
                         <div class="circle">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 377.06 377.06">
@@ -47,8 +52,14 @@
                     </div>
                 </div>
             </article>
-    @endforeach
+        @endforeach
+
+
+
+
     <!--/-->
+        <!--Item-->
+
 
     </div>
 </section>
